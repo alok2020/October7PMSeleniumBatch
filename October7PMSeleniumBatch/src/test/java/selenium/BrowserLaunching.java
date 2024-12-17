6990692f961fed271e028bfed4b544819517fb9a
GurpreetDel <gurpreetkaurchadhafan@gmail.com>
2024-12-17 23:49:23 +0530

    Initial Commit

---
+++ October7PMSeleniumBatch/src/test/java/selenium/BrowserLaunching.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserLaunching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.close();
+		driver.quit();
+		
+	}
+
+}
