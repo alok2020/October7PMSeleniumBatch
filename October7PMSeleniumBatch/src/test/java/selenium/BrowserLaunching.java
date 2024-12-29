4a6c9b8366b24182c6981dfb617995a3c429e1a8
alok2020 <alokkhare2020@gmail.com>
2024-12-29 22:57:43 +0530

    Added environment properties files and updated BaseTest

--- October7PMSeleniumBatch/src/test/java/selenium/BrowserLaunching.java
+++ October7PMSeleniumBatch/src/test/java/selenium/BrowserLaunching.java
@@ -1,6 +1,8 @@
 package selenium;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -9,12 +11,18 @@ public class BrowserLaunching {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		WebDriver driver;
 		WebDriverManager.chromedriver().setup();
 		
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		
+		//WebDriverManager.firefoxdriver().setup();
+		
+		//driver = new FirefoxDriver();
 		driver.close();
 		driver.quit();
-		//hey baby aww123456
+		//hey baby aww123456789
 		
 	}
 
---
+++ October7PMSeleniumBatch/src/test/java/selenium/TC_001.java
@@ -0,0 +1,12 @@
+package selenium;
+
+public class TC_001 extends BaseTest {
+
+	public static void main(String[] args)
+	{
+			launch("firefox");
+			navigateUrl("https://www.amazon.com");
+
+	}
+
+}
